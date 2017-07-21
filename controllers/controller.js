fa78670e0a06658e756a94d46a66176693f358df
Sakina Shivji <shivjisakina@gmail.com>
2017-07-21 07:40:06 -0400

    saved notes to db but they wont show upp :(

--- controllers/controller.js
+++ controllers/controller.js
@@ -137,9 +137,12 @@ router.post('/homes/:id', function (req, res) {
                     }
                     else {
                         // Or send the document to the browser
-                        res.send(doc);
+                        //res.send(doc);
+                        res.render('index', {name: doc});
                     }
                 });
+
+            res.redirect('/')
         }
     });
 
